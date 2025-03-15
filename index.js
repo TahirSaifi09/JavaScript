6ae1fd245842080721679aaee8379fdaf6957ed4
Tahir Saifi <tahirsaifi413@gmail.com>
2025-03-15 15:27:26 +0530

    Data type in JavaScript

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 // console.log("Hello World")
 
-const UserName = "Tahirsaifi09";
-let name = "Tahir";
-let age = 23;
+// const UserName = "Tahirsaifi09";
+// let name = "Tahir";
+// let age = 23;
 
-console.log("Hello " + UserName);
+// console.log("Hello " + UserName);
 
-console.log("your age is : " + age);
+// console.log("your age is : " + age);
+
+
+
+// 6 Types of Data Type
+
+// number 
+// const age = 23;
+
+//Big Int
+// const value = 121111111187567;
+// console.log(value)
+
+// String
+// const name = "Tahir";
+// console.log(name);
+
+// Boolean
+// const isRunning = true;
+// console.log(isRunning);
+
+// Undefined
+const hello = "";
+console.log(hello)
+
+// const data =null
+// console.log(data)
 
