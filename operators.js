4f14f647a1c342ea3e32b852133e9d7992a770ee
Tahir Saifi <tahirsaifi413@gmail.com>
2025-03-15 17:25:31 +0530

    Arthmetic, Assignment, Comparison Operators

---
+++ operators.js
@@ -0,0 +1,70 @@
+//  Operaters
+
+//* Arthmetic Operators
+// Addition (+), Subtraction (-), Multiplication(*), Division(/), and remainder(modules %)
+let sum = 30 + 20;
+// console.log(sum)
+
+let sub = 20 - 10;
+// console.log(sub)
+
+let mul = 10 * 3;
+// console.log( mul)
+
+let div = 10 / 3;
+// console.log(div);
+
+let rem = 10 % 3;
+// console.log(rem);
+
+//* Assignment Operator
+// =, +=, -=, *=, /=, %=
+
+let count = 5;
+count += 3;
+// console.log(count);
+
+let subs = 10;
+subs -= 6;
+// console.log(subs);
+
+// * Comparison Operators
+// ==(equal to) ===(exactly equal to), !=(not equal to), !==(not exactly equal to), <(less than), >(greater than)
+// <=(less than equal to) >=(greater than equal to) 
+
+
+const num1 =5;
+const num2 =10;
+
+console.log(num1==num2);
+if(num1==num2){
+    console.log("Both number is equal");
+}
+else{
+    console.log("Number is not equal")
+}
+
+const num3 = 3;
+const num4 = "3";
+console.log(num3 === num4)
+
+const letter = "A";
+console.log(letter == "A")
+
+const score = 10;
+if(score >= 90){
+    console.log("Your grade is A")
+}
+else if(score >= 80){
+    console.log("Your grade is B");
+}
+else if(score >= 60){
+    console.log("Your grade is C");
+}
+else if(score >= 40){
+    console.log("Pass");
+}
+else{
+    console.log("You are fail");
+}
+
