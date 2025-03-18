a552cace60b9a7f323d7ebf0dd522088509d2bb4
tahirsaifi09 <tahirsaifi413@gmail.com>
2025-03-19 00:18:31 +0530

    Function in javascript

---
+++ function.js
@@ -0,0 +1,47 @@
+// Function
+// Function is used for anywhere and anytime;
+
+// function name(){
+//     console.log("Hello");
+//     console.log("How");
+//     console.log("Are");
+//     console.log("you");
+// }
+
+// name();
+// name();
+// name();
+
+// Argument Or Input
+// return or output
+
+// function print(name){
+//     console.log("Hello , " + name);
+// }
+// // Input pass krenge
+// print("Tahir");
+// print("Deepak")
+
+// Multiple argument or input
+// function print(fname, lname){
+//     console.log("Hello , " + fname + " " + lname);
+// }
+// print("Tahir", "Saifi");
+// print("Deepak")   // Undefile show
+
+
+
+
+// Return or Output
+// function value(first, second){
+//     console.log(first + second)
+// }
+// value(3,5);
+
+function value(first, second){
+    return first + second;
+}
+const result = value (20,13);
+console.log(result);
+
+
